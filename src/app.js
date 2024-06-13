ab2f8e5f072d300f2d5b08d4169197ae4e3a12b6
Rakesh Choudhary <seervir987@gmail.com>
2024-06-13 21:05:41 +0530

    add the register controller

--- src/app.js
+++ src/app.js
@@ -16,4 +16,12 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// import Routes
+
+import userRouter from "./routes/user.routes.js";
+
+// Routes declaration
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
