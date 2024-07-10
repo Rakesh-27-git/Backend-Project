0e5e14da43ba703c138ea7031a56ea23a8bc7c41
Rakesh Choudhary <seervir987@gmail.com>
2024-07-10 22:38:52 +0530

    Add Subscription and Dashboard controller.

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { Like } from "../models/like.model.js";
@@ -7,11 +7,48 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id");
+  }
+
+  const channelSubscribers = await Subscription.find({ channel: channelId });
+  const totalSubscribers = channelSubscribers.length;
+
+  const channelVideos = await Video.find({ owner: channelId });
+  const totalVideos = channelVideos.length;
+
+  const videoIds = channelVideos.map((video) => video._id);
+  const totalViews = await Like.countDocuments({ video: { $in: videoIds } });
+
+  return res.status(200).json(
+    new ApiResponse(200, "Channel stats retrieved successfully", {
+      totalSubscribers,
+      totalVideos,
+      totalViews,
+    })
+  );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-  // TODO: Get all the videos uploaded by the channel
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id");
+  }
+
+  const channelVideos = await Video.find({ owner: channelId });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        "Channel videos retrieved successfully",
+        channelVideos
+      )
+    );
 });
 
 export { getChannelStats, getChannelVideos };
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,17 +7,88 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-  // TODO: toggle subscription
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id");
+  }
+  // check if channel exists
+  const channel = await User.findById(channelId);
+  if (!channel) {
+    throw new ApiError(404, "Channel not found");
+  }
+  // check if user is already subscribed
+  const existingSubscription = await Subscription.findOne({
+    subscriber: req.user._id,
+    channel: channelId,
+  });
+  if (existingSubscription) {
+    // unsubscribe
+    await Subscription.findByIdAndDelete(existingSubscription._id);
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Subscribed successfully"));
+  } else {
+    // subscribe
+    await Subscription.create({
+      subscriber: req.user._id,
+      channel: channelId,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Unsubscribed successfully"));
+  }
 });
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id");
+  }
+  // check if channel exists
+  const channel = await User.findById(channelId);
+  if (!channel) {
+    throw new ApiError(404, "Channel not found");
+  }
+  // get subscribers
+  const subscribers = await Subscription.find({ channel: channelId }).populate(
+    "subscriber",
+    "username"
+  );
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Subscribers retrieved successfully", subscribers)
+    );
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+  if (!isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "Invalid subscriber id");
+  }
+  // check if user exists
+  const user = await User.findById(subscriberId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+  // get subscribed channels
+  const subscribedChannels = await Subscription.find({
+    subscriber: subscriberId,
+  }).populate("channel", "username");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        "Subscribed channels retrieved successfully",
+        subscribedChannels
+      )
+    );
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
